11db66475a702278146d344360c405c82a771215
Jindrich Skupa <jindrich.skupa@gmail.com>
2021-10-14 12:32:42 +0200

    Update lambda code

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -9,14 +9,14 @@ def lambda_handler(event, context):
     msg = {
         "channel": os.environ['SLACK_CHANNEL'],
         "username": os.environ['SLACK_USERNAME'],
-        "text": event['Records'][0]['Sns']['Message'],
+        "text": event['Records'][0]['Sns']['Message']['AlarmName'] + ":" + event['Records'][0]['Sns']['Message']['AlarmDescription'],
         "icon_emoji": ""
     }
     
     encoded_msg = json.dumps(msg).encode('utf-8')
     resp = http.request('POST',url, body=encoded_msg)
     print({
-        "message": event['Records'][0]['Sns']['Message'], 
-        "status_code": resp.status, 
+        "message": event['Records'][0]['Sns']['Message']['AlarmName'] + ":" + event['Records'][0]['Sns']['Message']['AlarmDescription'],
+        "status_code": resp.status,
         "response": resp.data
     })
